eae3e12c0b5bc7805617c88c41774255b2214c46
romnoelp <petracorta.rmnl@gmail.com>
2024-10-25 12:15:48 +0800

    Finished the API integration

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -1,13 +1,35 @@
 import { Input } from "@/components/ui/input";
+import { useState } from "react";
+import {
+   Button
+   
+ } from "@/components/ui/button";
 
-const SearchBar = () => {
+type SearchBarProps = {
+   onSearch: (value: string) => void;
+};
+
+const SearchBar = (props: SearchBarProps) => {
+   const [input, setInput] = useState("");
+
+   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setInput(e.target.value);
+   };
+
+   const handleSearchClick = () => {
+      props.onSearch(input);
+   };
+
    return (
       <div className="w-full flex gap-4">
          <Input
             id="search-pokemon"
             type="text"
             placeholder="What pokemon are you looking for?"
+            value={input}
+            onChange={handleInputChange}
          />
+         <Button onClick={handleSearchClick}>Search</Button>
       </div>
    );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import SearchBar from "./components/SearchBar";
-import { Button } from "@/components/ui/button";
 import {
    Card,
    CardContent,
@@ -10,41 +9,72 @@ import {
    CardHeader,
    CardTitle,
 } from "@/components/ui/card";
-import { PokemonClient } from "pokenode-ts";
 import { useEffect, useState } from "react";
-import PokemonCard from "./components/PokemonCard";
-import { error } from "console";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
+type PokemonData = {
+   name: string;
+   height: number;
+   weight: number;
+};
 const Home = () => {
    const [searchedPokemon, setSearchedPokemon] = useState("");
-   const [pokemonResult, setPokemonResult] = useState([]);
-   const pokemonClient = new PokemonClient();
-   const handleSearchPokemon = () => {
-      console.log("Button clicked!");
+   const [pokemonResult, setPokmonResult] = useState<PokemonData>();
+
+   const handleSearchPokemon = (value: string) => {
+      setSearchedPokemon(value);
    };
 
    useEffect(() => {
-      fetch(`https://pokeapi.co/api/v2/pokemon/`)
-         .then((response) => response.json())
-         .then((data) => setPokemonResult(data))
-      .catch((error) => console.error(error))
+      const fetchPokemon = async () => {
+         try {
+            const response = await fetch(
+               `https://pokeapi.co/api/v2/pokemon/${searchedPokemon}`
+            );
+
+            if (!response.ok) {
+               throw new Error("Can't find the pokemon you provided");
+            }
+
+            const data = await response.json();
+            setPokmonResult(data);
+         } catch (error) {
+            console.error(error);
+         }
+      };
+      fetchPokemon();
    }, [searchedPokemon]);
 
    return (
       <div className="flex p-12">
          <div className="flex flex-col w-full gap-4">
             <h1 className="text-3xl font-bold">Pokedexed</h1>
-            <div className="flex gap-4">
-               <SearchBar />
-               <Button onClick={handleSearchPokemon}>Search</Button>
-            </div>
+            <SearchBar onSearch={handleSearchPokemon} />
             <Card>
                <CardHeader>
-                  <CardTitle></CardTitle>
-                  <CardDescription></CardDescription>
+                  <CardTitle>Here's the Pokemon/s you searched:</CardTitle>
+                  <CardDescription>
+                     The pokedex will show you various information about them.
+                  </CardDescription>
                </CardHeader>
                <CardContent>
-                  <PokemonCard name={"Pikachu"} description="test" />
+                  <ScrollArea className="h-72 w-full rounded-md border">
+                     {pokemonResult ? (
+                        <div>
+                           <p>
+                              <strong>Name:</strong> {pokemonResult.name}
+                           </p>
+                           <p>
+                              <strong>Height:</strong> {pokemonResult.height}
+                           </p>
+                           <p>
+                              <strong>Weight:</strong> {pokemonResult.weight}
+                           </p>
+                        </div>
+                     ) : (
+                        <p>No Pokémon found.</p>
+                     )}
+                  </ScrollArea>
                </CardContent>
                <CardFooter>
                   <p className="font-thin text-sm">Powered by Pokenode-ts</p>
